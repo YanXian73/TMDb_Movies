0acd38052f8c4024377cba1b69322aaa4e46d29c
jerry <efrtyjukopp@icloud.com>
2021-07-30 10:44:41 -0700

    移除ShowMovieViewController, 改用ShowMovieTableViewController 完成顯示電影詳細資料, 完成ScrollViewController的元件設定, 完成Collection cell 的顯示,  一排顯示3個ImageView

---
+++ MyTicketStub/Cell/ShowMovieTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShowMovieTableViewCell.swift
+//  MyTicketStub
+//
+//  Created by 吳彥賢 on 2021/7/29.
+//
+
+import UIKit
+
+class ShowMovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
